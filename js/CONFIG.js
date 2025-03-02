897cff7c86d73f9c3550981e448ace47ff6a03ac
trantienvn <tientn2005@gmail.com>
2025-03-02 11:34:20 +0700

    first commit

---
+++ js/CONFIG.js
@@ -0,0 +1,44 @@
+const __DATA__ = {
+
+    'cover-image': 't.jpg',
+    'avatar-image': 'chaotho.jpg',
+    'name': 'Xuân Thảo',
+    'skills': [
+        'Thích làm chủ tịch',
+        'Lớp trưởng',
+    ],
+    'bio-storys': [{
+        'image': 'xt.png',
+        'content': 'Giới thiệu',
+        'author': 'Chào Bạn 👋, có thể gọi tôi là Cháo Thỏ 🐰',
+    },],
+    'links': [{
+        'image': 'facebook.jpg',
+        'title': 'Facebook',
+        'show-value': 'Xuân Thảo',
+        'value': 'https://www.facebook.com/100033078738248/',
+        'type': 0,
+    },
+    {
+        'image': 'mess.png',
+        'title': 'Messenger',
+        'show-value': 'Xuân Thảo',
+        'value': 'https://m.me/100033078738248',
+        'type': 0,
+    },
+    {
+        'image': 'zalo.jpg',
+        'title': 'Zalo',
+        'show-value': 'Xuân Thảo',
+        'value': 'https://zalo.me/0326123861',
+        'type': 0,
+    },
+    {
+        'image': 'gmail.jpg',
+        'title': 'Gmail',
+        'show-value': 'dtc235200693@ictu.edu.vn',
+        'value': 'mailto:dtc235200693@ictu.edu.vn',
+        'type': 0,
+    }
+    ]
+};
